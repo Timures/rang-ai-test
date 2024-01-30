dafd769a241458da1cd815844ce8a2e136e52020
Timures <bisemsk@gmail.com>
2024-01-30 15:15:51 +0600

    add PeriodSeletor

--- app.vue
+++ app.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="container mx-auto">
+    <h1>Аналитика по Автозаводской</h1>
+    <PeriodSelector @periodChange="handlePeriodChange" />
+    <p v-if="selectedPeriod">Выбран период: {{ selectedPeriod }}</p>
   </div>
 </template>
+
+<script setup lang="ts">
+import PeriodSelector from '~/components/analytics/PeriodSelector.vue'
+import { ref } from 'vue'
+
+const selectedPeriod = ref('')
+const handlePeriodChange = (periodId: string) => {
+  selectedPeriod.value = periodId
+  // Тут ты можешь выполнить дополнительные действия при изменении периода
+}
+
+// Возможно, тут ты хочешь выполнить какие-то действия при изменении периода
+
+</script>
